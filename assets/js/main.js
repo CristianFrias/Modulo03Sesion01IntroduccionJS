8323b1451abdf61627b04476a0b53e34f8130edb
Cristian Frias <cristian.friasg@gmail.com>
2024-07-26 19:37:12 -0400

    Actualizando

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var apellido = "Frías"
+
+var persona = {
+    nombre: "Cristián", // cuando tenemos comillas lee la variable
+    apellido: apellido // aqui declaramos la variable antes
+}
+
+console.log(persona);
